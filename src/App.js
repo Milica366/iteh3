0312eb3f0220b9019af6691d0d702aa8c756afb8
Milica366 <svakiizborimasvojucenu@gmail.com>
2021-09-02 21:08:19 +0200

    zavrsena app

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/Navbar';
+import { Route, Switch } from 'react-router';
+import Spisak from './components/Spisak';
+import Kalkulator from './components/Kalkulator';
+
+
+function App() {
+  return (
+    <>
+      <Navbar />
+
+      <Switch>
+        <Route exact path='/'>
+          <Kalkulator />
+        </Route>
+        <Route path='/spisak'>
+          <Spisak />
+        </Route>
+
+      </Switch>
+    </>
+  );
+}
+
+export default App;
